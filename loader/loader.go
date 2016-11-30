4239dc8fcf277a19b8e029b144cc8b78f6015f14
Ivan Doroshenko <ivan.doroshenko@gettyimages.com>
2016-11-30 17:15:38 +0200

    comments removed

--- loader/loader.go
+++ loader/loader.go
@@ -23,25 +23,7 @@ func Get (url string) (*goquery.Document, error) {
     fmt.Println("goquery error")
     return nil, err;
   }
-  //result := make(map[string][]string)
-  //for name, query := range queries {
-  //  result[name] = ExtractFromDocument(doc, query[0], query[1], query[2])
-  //}
   return doc, nil
-
-  //doc.Find(query).Each(func(i int, s *goquery.Selection) {
-  //  var value string
-  //  switch prop {
-  //    case attrProp:
-  //      value, _ = s.Attr(name)
-  //    break
-  //    default:
-  //      value = s.Text()
-  //  }
-  //  result = append(result, value)
-  //  fmt.Printf("item %d: %s\n", i, value)
-  //})
-  //return result, nil
 }
 
 func ExtractFromDocument (doc *goquery.Document, query, prop, name string) []string {
